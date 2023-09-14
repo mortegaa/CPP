0df12df8077e5f612e969a0dad0100c6a7da28a3
mortega- <mortega-@student.42madrid.com>
2023-09-14 21:03:27 +0200

    Fino

--- M9/ex00/srcs/BitcoinExchange.cpp
+++ M9/ex00/srcs/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: mortega- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 19:59:32 by mortega-          #+#    #+#             */
-/*   Updated: 2023/09/12 20:22:54 by mortega-         ###   ########.fr       */
+/*   Updated: 2023/09/14 21:01:41 by mortega-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,9 +41,33 @@ BitcoinExchange::~BitcoinExchange()
 
 bool BitcoinExchange::validateFile( void )
 {
+	ifstream input(_file);
+	// TRYCATCH if (!input.is_open())
+	std::string line;
+	while (std::getline(input, line))
+	{
+		int pos = line.find(',');
+		std::string date = line.substr(0, pos);
+		std::strind value = line.substr(pos + 1, line.size());
+	}
+	input.close();
+
+}
+
+bool BitcoinExchange::validateFile(std::string file)
+{
+
+}
+
+bool BitcoinExchange::exchange( void )
+{
 
 }
 
 BitcoinExchange::
 BitcoinExchange::
 BitcoinExchange::
+
+
+
+
